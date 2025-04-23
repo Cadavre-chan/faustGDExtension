05844d4b7c75a83a1c240e4f50b91983a832df46
Cadavre-chan <axel.chypre@epitech.eu>
2025-04-23 16:26:35 +0200

    Extension refacto to turn it into an AudioEffect (WIP)

--- src/faust2Godot.cpp
+++ src/faust2Godot.cpp
@@ -14,7 +14,22 @@
 using namespace godot;
 
 Faust2Godot::Faust2Godot() {
-    UtilityFunctions::print("Constructing extension!");
+    UtilityFunctions::print("Starting initialization!");
+
+    // general setup
+    input = new float*[2];
+    input[0] = new float[1024];
+    input[1] = new float[1024];
+
+    output = new float*[2];
+    output[0] = new float[1024];
+    output[1] = new float[1024];
+
+    sampleRate = AudioServer::get_singleton()->get_mix_rate();
+    this->loadDSP();
+    this->loadDSPLibrary();
+    this->dspObject = this->createDSP();
+    this->init_dsp(dspObject, sampleRate);
 }
 
 Faust2Godot::~Faust2Godot() {
@@ -35,103 +50,22 @@ Faust2Godot::~Faust2Godot() {
     }
 }
 
-void Faust2Godot::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("init_ext"), &Faust2Godot::initExtension);
-    ClassDB::bind_method(D_METHOD("process"), &Faust2Godot::process);
-}
+// void Faust2Godot::_bind_methods() {
+//     ClassDB::bind_method(D_METHOD("process"), &Faust2Godot::process);
+// }
 
-void Faust2Godot::initExtension() {
-    UtilityFunctions::print("Starting initialization!");
-
-    // general setup
-    input = new float*[2];
-    input[0] = new float[20512];
-    input[1] = new float[20512];
-
-    output = new float*[2];
-    output[0] = new float[512];
-    output[1] = new float[512];
-
-    currentTime = godot::Time::get_singleton();
-
-    server = AudioServer::get_singleton();
-    sampleRate = server->get_mix_rate();
-    this->loadDSP();
-    this->loadDSPLibrary();
-    this->dspObject = this->createDSP();
-    this->init_dsp(dspObject, sampleRate);
-
-    // record bus setup
-
-    recordBusIndex = server->get_bus_count();
-    server->add_bus(recordBusIndex);
-    server->set_bus_name(recordBusIndex, "recordBus");
-    capture.instantiate();
-    server->add_bus_effect(recordBusIndex, capture);
-    server->set_bus_send(recordBusIndex, "Master");
-
-    capturePlayer = memnew(AudioStreamPlayer);
-    capturePlayer->set_name("recordBusPlayer");
-    capturePlayer->set_bus("recordBus");
-    server->set_bus_volume_db(recordBusIndex, 0);
-    
-    this->add_child(capturePlayer);
-    
-    captureStream = ResourceLoader::get_singleton()->load("res://assets/file_example_WAV_10MG.wav");
-    if (!captureStream.is_valid()) {
-        UtilityFunctions::print("Stream is invalid!");
+void Faust2Godot::process(const AudioFrame *src, AudioFrame *dst, int frame_count) {
+    for (int i = 0; i < frame_count; i++) {
+        input[0][i] = src[i].left;
+        input[1][i] = src[i].right;
     }
-    capturePlayer->set_stream(captureStream);
-    capturePlayer->play();
 
-    // playback bus setup
+    compute(dspObject, frame_count, input, output);
 
-    playbackBusIndex = server->get_bus_count();
-    server->add_bus(playbackBusIndex);
-    server->set_bus_name(playbackBusIndex, "playbackBus");
-    server->set_bus_send(playbackBusIndex, "Master");
-
-    playbackGenerator.instantiate();
-    playbackGenerator->set_mix_rate(44100);
-    playbackGenerator->set_buffer_length(0.2);
-
-    playbackPlayer = memnew(AudioStreamPlayer);
-    playbackPlayer->set_stream(playbackGenerator);
-    playbackPlayer->set_bus("playbackBus");
-    this->add_child(playbackPlayer);
-    playbackPlayer->play();
-
-    
-    playbackStreamGenerator = playbackPlayer->get_stream_playback();
-
-    startTime = currentTime->get_unix_time_from_system();
-}
-
-void Faust2Godot::process() {
-    double engine_time = currentTime->get_unix_time_from_system();
-    // UtilityFunctions::print("Time delta from last frame (s): ", (engine_time - startTime) - (lastFrameTime - startTime));
-    lastFrameTime = engine_time;
-    int maxBuffer = 1024;
-    int available = capture->get_frames_available();
-    PackedVector2Array buffer = capture->get_buffer(MIN<int, int>(maxBuffer, available));
-    int bufferSize = buffer.size();
-    UtilityFunctions::print("Buffer size: ", bufferSize);
-    if (bufferSize == 0) {
-        UtilityFunctions::print("Empty buffer, skipping process.");
-        return;  
+    for (int i = 0; i < frame_count; i++) {
+        dst[i].left = output[0][i];
+        dst[i].right = output[1][i];
     }
-    
-    vector2ArrayToFloats(buffer, bufferSize, input);
-    
-    
-    this->compute(this->dspObject, bufferSize, input, output);
-    UtilityFunctions::print("Frames available: ", playbackStreamGenerator->get_frames_available());
-    this->playbackStreamGenerator->push_buffer(buffer);
-
-    // for (int i = 0; i < buffer.size(); i++) {
-    //     UtilityFunctions::print("Amplitude left: ", Math::abs(output[0][i]), " Amplitude right: ", Math::abs(output[1][i]));
-    // }
-
 }
 
 void Faust2Godot::loadDSPLibrary() {
@@ -152,23 +86,5 @@ void Faust2Godot::loadDSP() {
     this->_dsp = dlopen(dspPath.c_str(), RTLD_LAZY);
     if (!this->_dsp) {
         UtilityFunctions::print("Failed to load DSP.\n");
-    }
-}
-
-PackedVector2Array Faust2Godot::floatsToVector2Array(float **channels, int size) {
-    PackedVector2Array array;
-
-    for (int i = 0; i < size; i++) {
-        array.push_back(Vector2(channels[0][i], channels[1][i]));
-    }
-
-    return array;
-}
-
-void Faust2Godot::vector2ArrayToFloats(PackedVector2Array array, int size, float **channels) {
-
-    for (int i = 0; i < size; i++) {
-        channels[0][i] = array[i].x;
-        channels[1][i] = array[i].y;
     }
 }
--- src/faust2Godot.h
+++ src/faust2Godot.h
@@ -16,38 +16,11 @@
 
 namespace godot {
 
-class Faust2Godot : public godot::Node {
-    GDCLASS(Faust2Godot, godot::Node);
+class Faust2Godot : public godot::AudioEffect {
+    GDCLASS(Faust2Godot, godot::AudioEffect);
 
     private:
-        godot::Time *currentTime;
-
-        double startTime = 0;
-        double lastFrameTime = 0;
-
-        AudioServer * server = NULL; // pointer to Godot AudioServer singleton
-
         float sampleRate; // AudioServer sample rate
-
-        // record bus
-
-        int recordBusIndex; // index of the bus using the capture effect
-
-        Ref<AudioEffectCapture> capture; // current instance of the capture effect
-
-        Ref<AudioStream> captureStream; // audio source for the capture bus
-
-        AudioStreamPlayer *capturePlayer = NULL; // pointer to the object playing the audio
-
-        // playback bus
-
-        int playbackBusIndex;
-
-        Ref<AudioStreamGeneratorPlayback> playbackStreamGenerator;
-
-        Ref<AudioStreamGenerator> playbackGenerator; // apparently this is needed for the variable above
-
-        AudioStreamPlayer *playbackPlayer = NULL;
 
         // DSP stuff
 
@@ -71,19 +44,11 @@ class Faust2Godot : public godot::Node {
         float **input = NULL;
         float **output = NULL;
 
-        // Utility
-
-		PackedVector2Array floatsToVector2Array(float **list, int size);
-
-		void vector2ArrayToFloats(PackedVector2Array array, int size, float **channels);
-
     public:
         Faust2Godot();
         ~Faust2Godot();
 
-        void initExtension();
-
-        void process(); // function called at every cycle
+        void process(const AudioFrame *src, AudioFrame *dst, int frame_count); // function called at every cycle
 
         static void _bind_methods();
 };
